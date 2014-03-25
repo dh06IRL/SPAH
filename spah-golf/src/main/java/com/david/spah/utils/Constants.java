d358fc31b9047eafb4e72677504c4cdbda35b8f4
David Hodge <dhodge.jr220@gmail.com>
2014-03-24 20:07:19 -0400

    quick constants build out

---
+++ spah-golf/src/main/java/com/david/spah/utils/Constants.java
@@ -0,0 +1,12 @@
+package com.david.spah.utils;
+
+/**
+ * Created by david.hodge on 3/24/14.
+ */
+public class Constants {
+
+    public static final String EVENT_WEBSITE = "http://spahagolftournament.webs.com/";
+    public static final String EVENT_RSS = "http://spahagolftournament.webs.com/apps/blog/entries/feed/rss";
+    public static final String OWM_BASE_URL = "http://api.openweathermap.org/data/2.5/weather?";
+    public static final String HOLES_BASE_URL = "http://www.vbnational.com/images/";
+}
